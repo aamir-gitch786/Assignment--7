6811f9252527bb89862b9d0e294687a6d169d5a8
MD AAMIR <111450941+aamir-gitch786@users.noreply.github.com>
2023-01-30 00:26:53 +0530

    Update 3. Write a program to check whether a given number is there in the Fibonacci.c

--- 3. Write a program to check whether a given number is there in the Fibonacci.c
+++ 3. Write a program to check whether a given number is there in the Fibonacci.c
@@ -10,19 +10,12 @@ series or not.
 
 int main()
 {
-    int n,sqrt_one,sqrt_two,r1=0,r2=0;
+    int n,sqrt_one,sqrt_two;
     printf("Enter the number\n");
     scanf("%d",&n);
-    
     sqrt_one=sqrt(5*n*n+4);
-    if((5*n*n+4)==pow(sqrt_one,2))
-    r1=1;
-    
     sqrt_two=sqrt(5*n*n-4);
-    if((5*n*n-4)==pow(sqrt_two,2))
-    r2=1;
-    
-    if(r1|| r2)
+    if((5*n*n+4)==pow(sqrt_one,2) || (5*n*n-4)==pow(sqrt_two,2) )
     printf("Yes \n");
     else
     printf("No\n");
