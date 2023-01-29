6d840758892bd37264b48bcd7cfa704b9a3179b6
MD AAMIR <111450941+aamir-gitch786@users.noreply.github.com>
2023-01-29 15:53:38 +0530

    Update 9. Write a program to check whether a given number is an Armstrong number.c

--- 9. Write a program to check whether a given number is an Armstrong number.c
+++ 9. Write a program to check whether a given number is an Armstrong number.c
@@ -7,26 +7,29 @@ or not
 #include<math.h>
 
 int main()
-{  int n,count=0,tem,tem1,sum=0;
-printf("Enter the number \n");
-scanf("%d",&n);
-tem=n;
-tem1=n;
-while(n!=0)
 {
-  n/=10;
-  count++;
-}
-while(tem!=0)
-{
-    sum=sum+pow(tem%10,count);
-    tem=tem/10;
-}
-if(sum==tem1)
-printf("%d is a Armstrong number",tem1);
-else
-printf("%d is not a Armstrong number",tem1);
-
-
+  int n,digit=0,tem,sum=0,result_matching;
+  printf("Enter the number\n");
+  scanf("%d",&n);
+  tem=n;
+  result_matching=n;
+  while(n)//for finding the number of digits in given number.
+  {
+    n/=10;
+    digit++;
+  }
+  while(tem)//for suming the values.
+  {
+      sum=sum+pow(tem%10,digit);
+      tem/=10;
+  }
+  if(sum==result_matching)//for checking the conditions
+  printf("Yes %d is a Armstrong number\n",result_matching);
+  else
+  printf("No %d is not a Armstrong number\n",result_matching);
+  
+    
     return 0;
 }
+
+
