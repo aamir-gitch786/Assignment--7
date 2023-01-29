edd81e92e9980c70d6b47eb71d5935c27918779d
MD AAMIR <111450941+aamir-gitch786@users.noreply.github.com>
2023-01-29 14:07:12 +0530

    Update 3. Write a program to check whether a given number is there in the Fibonacci.c

--- 3. Write a program to check whether a given number is there in the Fibonacci.c
+++ 3. Write a program to check whether a given number is there in the Fibonacci.c
@@ -6,23 +6,26 @@ series or not.
 *******************************************************************************/
 
 #include <stdio.h>
+#include<math.h>
 
 int main()
 {
-    int a=-1,b=1,c,n,i;
-    printf("Enter the number that you want to check in the Fibonacci seires :\n");
+    int n,sqrt_one,sqrt_two,r1=0,r2=0;
+    printf("Enter the number\n");
     scanf("%d",&n);
-   
-    for( i=1;i<=2*n+1;i++)
-    {
-        c=a+b;
-        if(c==n)
-        {printf("Yes it is present in the Fibonacci seires ");
-        return 0;}
-        a=b;
-        b=c;
-    }
-    if(c!=n)
-    printf("No it is not present in the Fibonacci seires");
+    
+    sqrt_one=sqrt(5*n*n+4);
+    if((5*n*n+4)==pow(sqrt_one,2))
+    r1=1;
+    
+    sqrt_two=sqrt(5*n*n-4);
+    if((5*n*n-4)==pow(sqrt_two,2))
+    r2=1;
+    
+    if(r1|| r2)
+    printf("Yes \n");
+    else
+    printf("No\n");
+    
     return 0;
 }
