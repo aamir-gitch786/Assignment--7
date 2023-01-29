47947e3d0cfabe1adf2e983b666db5e365b2fadd
MD AAMIR <111450941+aamir-gitch786@users.noreply.github.com>
2023-01-29 14:17:18 +0530

    Update 4. Write a program to calculate HCF of two numbers.c

--- 4. Write a program to calculate HCF of two numbers.c
+++ 4. Write a program to calculate HCF of two numbers.c
@@ -3,23 +3,21 @@
 *******************************************************************************/
 
 #include <stdio.h>
+#include<math.h>
 
 int main()
 {
-    int a,b,ans,minimum;
-    printf("Enter the value of two numbers\n");
-    scanf("%d%d",&a,&b);
-   
-    minimum=a>b?b:a;
-    for(int i=1;i<=minimum;i++)
-    {
-        if(a%i==0 && b%i==0)
-        {
-            ans=i;
-        }
-    }
-    printf("HCF of %d and %d is : %d",a,b,ans);
-    
+   int a,b,min;
+   scanf("%d%d",&a,&b);
+   min=a<b?a:b;
+   for(int i=min;i>=1;i--)
+   {
+       if(a%i==0 && b%i==0)
+       {printf("HCF of %d and %d is %d",a,b,i);
+       break;
+       }
+   }
     
     return 0;
 }
+
